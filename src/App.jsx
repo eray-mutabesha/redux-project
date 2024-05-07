25e22aa80b1ee19f592f8a0377c011d9d3ad0ed2
eray-mutabesha <107131060+eray-mutabesha@users.noreply.github.com>
2024-05-07 01:12:53 -0700

    comment

--- src/App.jsx
+++ src/App.jsx
@@ -4,8 +4,9 @@ import './App.css'
 import Form from './components/Form'
 
 function App() {
+  // a comment that describe the userselector
  const posts = useSelector((state)=>state.postReducer);
- console.log(posts)
+ 
   return (
     <>
        <Form/>
