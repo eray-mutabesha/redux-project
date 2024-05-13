8965598eb978ac563fa3ff4fb7ccfd570d7b7f47
eray-mutabesha <107131060+eray-mutabesha@users.noreply.github.com>
2024-05-13 13:03:41 -0700

    remove ed

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,30 +1,17 @@
 import React from 'react'
-import { useState } from 'react'
-
-
 
 
 const Post=({post})=> {
-
-
   return (
    
     <div className='all_post_div'>
      <h1>{post.titre}</h1>
      
-    {editpost ?(
-      <form action="">
-        <textarea autoFocus={true}  placeholder='edit' defaultValue={post.post}>
-
-        </textarea>
-        <button type='submit'>valder</button>
-      </form>
-    ):<p>{post.post}</p>}
+    <p>{post.post}</p>
     
     <div className='like_and_delete_images_div'>
      <img src="public\images\black_like.png" alt="likeImage" />
      <img src="public\images\deleteimage.png" alt="likeImage" />
-     {/* <img src="public\images\editPhoto.png"/> */}
     </div>
     </div>
     
